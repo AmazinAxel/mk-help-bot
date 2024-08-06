ec2caf64de8dc134a043ab158755e4207ad5e940
AmazinAxel <ars18451@gmail.com>
2024-08-06 16:32:30 -0700

    add embedded article links

--- src/articles.js
+++ src/articles.js
@@ -16,7 +16,7 @@ export const articleItems = [
 export const articles = [
     // Can't connect
     `### Can't connect to Minekeep
-    content goes here (\`this works\` *(and even **this!**)*)`,
+    If you're experiencing connection issues to `,
 
     // Cracked accounts
     `### Cracked accounts are __not__ supported on Minekeep
@@ -33,8 +33,9 @@ export const articles = [
     Cracked accounts cannot be used to link Minekeep accounts [more info about why here]`,
 
     // 1.21 support
-    `### 1.21 support is experimental
-    Use 1.21 builds with caution! [more info here]`,
+    `### 1.21 support is now stable
+    You may change your server version to 1.21 for additional features.
+    All Paper 1.21 builds are now stable so you may play 1.21 without the worry of crashes and data loss.`,
 
     // Bedrock support
     `### Bedrock support is currently broken
@@ -55,4 +56,36 @@ export const articles = [
     // Servers are not 24/7
     `### Minekeep servers can be started at any time
     However, they are __not__ 24/7. [more info here]`
-]
+];
+
+export const articleLinks = [
+    // Can't connect TODO write an article for this
+    'https://mkdocs.subgplayz.me/faq/#can-i-join-using-a-cracked-account',
+
+    // Cracked accounts
+    'https://mkdocs.subgplayz.me/faq/#can-i-join-using-a-cracked-account',
+    
+    // How to make a server
+    'https://mkdocs.subgplayz.me/account/server-creation/',
+
+    // Linking your account
+    'https://mkdocs.subgplayz.me/account/server-creation/',
+
+    // 1.21 support
+    'https://papermc.io/downloads/paper',
+
+    // Bedrock support
+    'https://mkdocs.subgplayz.me/faq/#can-i-join-on-bedrock',
+
+    // Mod support
+    'https://mkdocs.subgplayz.me/faq/#can-i-add-forge-fabric-mods-to-my-server',
+
+    // How to install plugins
+    'https://mkdocs.subgplayz.me/server-guides/adding-plugins/',
+
+    // Staff application policy
+    'https://mkdocs.subgplayz.me/faq/#where-can-i-apply-for-staff',
+
+    // Servers are not 24/7
+    'https://mkdocs.subgplayz.me/faq/#are-minekeep-servers-online-24-7'
+];
